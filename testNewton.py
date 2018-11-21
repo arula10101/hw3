67b469f1ebc200f5449d56ffc1f3a90898cca9a2
Laura Leal <lleal@princeton.edu>
2018-11-21 13:34:06 -0500

    Added testRadius and testStep to testNewton.py

--- testNewton.py
+++ testNewton.py
@@ -8,12 +8,8 @@ import newton
 class TestNewton(unittest.TestCase):
     def testLinear(self):
         f = lambda x : 3.0*x + 6.0
-
-        # Setting maxiter to 2 b/c we're guessing the actual root
         solver = newton.Newton(f, tol=1.e-15, maxiter=2)
         x = solver.solve(-2.0)
-        # Equality should be exact if we supply *the* root, ergo
-        # assertEqual rather than assertAlmostEqual
         self.assertEqual(x, -2.0)
 
     def testQuadratic(self):
@@ -22,7 +18,6 @@ class TestNewton(unittest.TestCase):
         solver = newton.Newton(f,tol=1.e-15,maxiter=200)
         x = solver.solve(3.3)
         self.assertAlmostEqual(x,3.0)
-       
 
     def testQuadratic2(self):
         # y : x*x + 6*x + 9
@@ -46,12 +41,27 @@ class TestNewton(unittest.TestCase):
         x = solver.solve(0.0)
         self.assertAlmostEqual(x,1.0)
 
-    def test2d(self):
+    def test2D(self):
         f = lambda x: np.matrix([[x[0,0]-x[1,0]+7],[x[0,0]+x[1,0]-15]])
         solver = newton.Newton(f, tol=1.e-6, maxiter=30)
         x0 = np.matrix([[3],[2]])
         x = solver.solve(x0)
         np.testing.assert_array_almost_equal(x, np.matrix([[4.], [11.]]))
+
+    def testRadius(self):
+        f = lambda x: x/(x**2 + 1.)
+        solver = newton.Newton(f, tol=1.e-15, maxiter=30, max_radius=1)
+        x = solver.solve(0.4)
+        self.assertAlmostEqual(x, 0.)
+        
+    def testStep(self):
+        f = lambda x : np.cos(x)
+        solver = newton.Newton(f,tol=1.e-15, maxiter=1)
+        x0 = 3
+        x1 = solver.step(x0)
+        x2 = solver.step(x0, f(x0))
+        self.assertEqual(x1, x2)
+
 
 if __name__ == "__main__":
     unittest.main()
