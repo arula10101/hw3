2eee1519f5599713221c682f3b498b5979157c50
Laura Leal <lleal@princeton.edu>
2018-11-21 14:00:49 -0500

    Added new tests to testNewton.py

--- testNewton.py
+++ testNewton.py
@@ -26,15 +26,7 @@ class TestNewton(unittest.TestCase):
         solver = newton.Newton(f, tol=1.e-15, maxiter=200,Df=_Df)
         x = solver.solve(-2.0)
         self.assertAlmostEqual(x, -3.0)
-        
-    def testExponential(self):
-        # f(x) = e^x
-        f = lambda x: np.exp(x)
-        solver = newton.Newton(f,tol=1.e-15,maxiter=50)
-        x = solver.solve(1.0)
-        self.assertEqual(x,float('inf'))
-#EXPONENTIAL IS NOT WORKING SO WELL
-
+       
     def testZeroDerivative(self):
         f = F.Polynomial([-1,0,1])
         solver = newton.Newton(f,tol=1.e-15,maxiter=200)
@@ -62,6 +54,25 @@ class TestNewton(unittest.TestCase):
         x2 = solver.step(x0, f(x0))
         self.assertEqual(x1, x2)
 
+    def testTangent(self):
+        f = lambda x: x*(1-x)
+        solver = newton.Newton(f,tol=1.e-15,maxiter=200)
+        x = solver.solve(0.5)
+        self.assertAlmostEqual(x,1.0)
+
+    def testNoRoot(self):
+        print("Expect this test to return maxiter error")
+        f = lambda x: x**2 + 1.0
+        solver = newton.Newton(f,tol=1.e-15,maxiter=200)
+        x = solver.solve(2.0)
+        print("Expect this test to return maxiter error")
+
+    def testBoundRoot(self):
+        print("Initial guess has to be within (-1/sqrt(2) , 1/sqrt(2))")
+        f = lambda x: x*np.exp(-x**2)
+        solver = newton.Newton(f,tol=1.e-15,maxiter=200,max_radius=1./np.sqrt(2.))
+        x = solver.solve(0.05)
+        self.assertAlmostEqual(x,0.0)
 
 if __name__ == "__main__":
     unittest.main()
